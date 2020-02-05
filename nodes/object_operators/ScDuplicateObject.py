6e1781bcb1fff679d30d993814c9d53b42408b79
Tomoaki Nakano <kanotomo@users.noreply.github.com>
2020-02-06 08:40:53 +0900

    Fixed Duplicate and Input nodes creating objects only once in a for-loop

--- nodes/object_operators/ScDuplicateObject.py
+++ nodes/object_operators/ScDuplicateObject.py
@@ -1,27 +1,47 @@
 import bpy
 
-from bpy.props import FloatProperty
+from bpy.props import FloatProperty, StringProperty
 from bpy.types import Node
 from .._base.node_base import ScNode
 from .._base.node_operator import ScObjectOperatorNode
-from ...helper import remove_object
+from ...helper import remove_object, print_log
 
 class ScDuplicateObject(Node, ScObjectOperatorNode):
     bl_idname = "ScDuplicateObject"
     bl_label = "Duplicate Object"
+
+    generated_objects: StringProperty(default="[]")
     
     def init(self, context):
         super().init(context)
         self.outputs.new("ScNodeSocketObject", "Duplicate Object")
     
+    def copy(self, node):
+        super().copy(node)
+        self.generated_objects = "[]"
+    
     def pre_execute(self):
-        remove_object(self.outputs["Duplicate Object"].default_value)
+        # Check first_time to prevent unexpected deletion inside for-loop
+        if (self.first_time):
+            for obj in eval(self.generated_objects):
+                # When obj is already deleted, remove_object() raises some exception.
+                try:
+                    remove_object(eval(obj))
+                except:
+                    print_log(self.name, None, "pre_execute",
+                              "Invalid object: " + repr(obj))
+            self.generated_objects = "[]"
         super().pre_execute()
     
     def functionality(self):
         bpy.ops.object.duplicate()
     
     def post_execute(self):
+        # Store duplicated object to delete it before re-execute this node.
+        tmp = eval(self.generated_objects)
+        tmp.append(repr(bpy.context.active_object))
+        self.generated_objects = repr(tmp)
+
         out = super().post_execute()
         out["Duplicate Object"] = bpy.context.active_object
         return out
